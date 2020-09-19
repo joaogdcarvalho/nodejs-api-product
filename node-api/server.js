5e81f646baf4aee242dcd86a36e7f49735b8d170
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-18 23:03:25 -0300

    conectando database

--- node-api/server.js
+++ node-api/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// iniciando o app
 const app = express();
 
+// iniciando o database
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  { useNewUrlParser: true }
+);
+
+// primeira rota
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
