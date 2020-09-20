6debbc80336b6798b71a373ff18071dd7faf55a2
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-19 23:40:29 -0300

    criando model de produto

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o app
 const app = express();
@@ -9,10 +10,18 @@ mongoose.connect(
   'mongodb://localhost:27017/nodeapi', 
   { useNewUrlParser: true }
 );
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // primeira rota
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native' });
+    
+  return res.send('Hello World!');
 });
 
 app.listen(3001);
