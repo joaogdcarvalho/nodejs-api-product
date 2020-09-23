8cd4244b2231bf6936f74cf402c9e22f8f6eebe9
joaogab <joaogabrieldecarvalho.com@gmail.com>
2020-09-22 21:57:25 -0300

    reestruturação de arquivos

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-// primeira rota
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native' });
-    
-  return res.send('Hello World!');
-});
+// rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
